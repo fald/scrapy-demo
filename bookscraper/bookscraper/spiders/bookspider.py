4ae5d368d8c98c4ba95dce442a48b89941b17369
fald <4348649+fald@users.noreply.github.com>
2023-07-15 18:58:13 -0400

    Spider tweaking when refreshing self.

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -15,22 +15,27 @@ class BookspiderSpider(scrapy.Spider):
             #     'price' : book.css(".product_price .price_color::text").get(),
             #     'url' : book.css("h3 a").attrib["href"]
             # }
-        # And this adds those follow URLS to a callback list.
-        # next_page = response.css("li.next a").attrib['href']
-        # if next_page:
-        #     next_url = "http://books.toscrape.com/"
-        #     if 'catalogue/' in next_page:
-        #         next_url += next_page
-        #     else:
-        #         next_url += "catalogue/" + next_page
-        #     yield response.follow(next_url, callback=self.parse)
-        
-            relative_url = book.css("h3 a").attrib["href"].get()
+            
+            # This instead grabs each book's url and then follows it with the different callback.
+            # What to do with each book when scraped from the catalogue.
+            relative_url = book.css("h3 a").attrib["href"]
             if 'catalogue/' in relative_url:
                 book_url = "http://books.toscrape.com/" + relative_url
             else:
                 book_url = "http://books.toscrape.com/catalogue/" + relative_url
+            # Not parsing the results pages with these, so we want a separate parser.
             yield response.follow(book_url, callback=self.parse_book_page)
+            
+        # This takes the next page buttons, follows them with the same parse method,
+        # then adds the result to the yielded generator.
+        next_page = response.css("li.next a").attrib['href']
+        if next_page:
+            next_url = "http://books.toscrape.com/"
+            if 'catalogue/' in next_page:
+                next_url += next_page
+            else:
+                next_url += "catalogue/" + next_page
+            yield response.follow(next_url, callback=self.parse)
             
     def parse_book_page(self, response):
         pass
